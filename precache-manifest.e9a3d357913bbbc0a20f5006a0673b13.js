ce5827591e67a3e6525a6509dd78f63fbfb9fea1
pikktorr <50754130+pikktorr@users.noreply.github.com>
2019-09-23 19:08:39 +0200

    Updates

---
+++ precache-manifest.e9a3d357913bbbc0a20f5006a0673b13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30492d02f89eb5c3ee4f91a45c32fe66",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "4a206bb18244269a0197",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "363c5796cccc5110a401",
+    "url": "/robofriends/static/css/main.680662b3.chunk.css"
+  },
+  {
+    "revision": "4a206bb18244269a0197",
+    "url": "/robofriends/static/js/2.6ba9d8b8.chunk.js"
+  },
+  {
+    "revision": "363c5796cccc5110a401",
+    "url": "/robofriends/static/js/main.b100da6e.chunk.js"
+  },
+  {
+    "revision": "d7d3c3ad781ab5e09c65",
+    "url": "/robofriends/static/js/runtime-main.a9498424.js"
+  }
+]);
